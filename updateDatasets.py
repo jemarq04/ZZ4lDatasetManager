460637b240476a32238cfaca5745daede3ecc789
hehe <he.he@cern.ch>
2022-05-26 17:20:43 -0400

    Recent updates related to tests

--- updateDatasets.py
+++ updateDatasets.py
@@ -6,6 +6,7 @@ parser.add_option("-y", dest="year",help="Which year")
 parser.add_option("-i", dest="inputname",help="Input json file name")
 parser.add_option("-o", dest="outputname",help="Output json file name")
 parser.add_option("--folder", dest="folder",help="folder containing new data files")
+parser.add_option("--noExtra", dest="noExtra",action='store_true',help="No extra MC/Data files")
 (options,args)=parser.parse_args()
 
 count =0
@@ -22,8 +23,9 @@ for key in datasets:
     if not key in obj.keys():
         print("Current file does not contain %s"%key)
         obj[key] = {}
-        if "HToZZ" in key or "HZZ" in key:
-            obj[key]['plot_group'] = "HZZ_signal"
+        #Temporary method for updating Higgs, disabled normally
+        #if "HToZZ" in key or "HZZ" in key:
+        #    obj[key]['plot_group'] = "HZZ_signal"
  
 for folder in options.folder.split(","):
 
@@ -34,15 +36,24 @@ for folder in options.folder.split(","):
 
     print(dirlist)
 
-    for item in datasets:
+    for item in datasets: # restrict MC keys to those in the list above
         for dirname in dirlist:
             if item in dirname:
               obj[item]["file_path"]=os.path.join(root,dirname)+"/*"
               count +=1
 
+    for dirname in dirlist: #handle data part
+        if 'data' in dirname:
+            for key in obj.keys():
+                if key in dirname:
+                    obj[key]["file_path"]=os.path.join(root,dirname)+"/*"
+                    count +=1
+
 #cleanup
 for key in obj.keys():
     if not key in datasets and not "data" in key:
+        del obj[key]
+    if obj[key] == {}:
         del obj[key]
 
 with open('FileInfo/ZZ4l%s/%s'%(options.year,options.outputname),'w') as output_file:
@@ -59,11 +70,12 @@ for key in obj_mc.keys():
     if "data" in key:
         del obj_mc[key]
 
-with open('FileInfo/ZZ4l%s/%s'%(options.year,options.outputname+"_dataOnly"),'w') as output_file:
-    json.dump(obj_data,output_file,indent=4)
+if not options.noExtra:
+    with open('FileInfo/ZZ4l%s/%s'%(options.year,options.outputname+"_dataOnly"),'w') as output_file:
+        json.dump(obj_data,output_file,indent=4)
 
-with open('FileInfo/ZZ4l%s/%s'%(options.year,options.outputname+"_MCOnly"),'w') as output_file:
-    json.dump(obj_mc,output_file,indent=4)
+    with open('FileInfo/ZZ4l%s/%s'%(options.year,options.outputname+"_MCOnly"),'w') as output_file:
+        json.dump(obj_mc,output_file,indent=4)
 
 print("Dataset count:%s"%count)
 print("New file produced:%s"%'FileInfo/ZZ4l%s/%s'%(options.year,options.outputname))
