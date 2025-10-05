8580d68a4cbcc230dbb5c3600620848c20dd084d
Justin Marquez <justin.marquez@wisc.edu>
2025-10-05 14:02:26 -0500

    add initial files for 2024 merging and plotting

---
+++ FileInfo/ZZ4l2024/makeJSON.sh
@@ -0,0 +1,92 @@
+year=2024
+srcdir=/hdfs/store/user/marquez/ZZ4l${year}-skimmed
+overwrite=true
+
+[[ -d $1 ]] && srcdir=$1
+srcdir=${srcdir%/}
+
+sims=(
+  #member_name,campaign_prefix,plot_group
+  zz4l-powheg,ZZto4L,qqZZ-powheg
+  zzjj4l-ewk,ZZJJto4L,qqZZjj-ewk
+  ggHZZ,GluGluH-Hto2Zto4L,HZZ-signal
+#  ggZZ4e,GluGlu*Continto2Zto4E,ggZZ #not yet available for 2024
+#  ggZZ4m,GluGlu*Continto2Zto4Mu,ggZZ #not yet available for 2024
+#  ggZZ4t,GluGlu*Continto2Zto4Tau,ggZZ #not yet available for 2024
+#  ggZZ2e2mu,GluGlu*Continto2Zto2E2Mu,ggZZ #not yet available for 2024
+#  ggZZ2e2tau,GluGlu*Continto2Zto2E2Tau,ggZZ #not yet available for 2024
+#  ggZZ2mu2tau,GluGlu*Continto2Zto2Mu2Tau,ggZZ #not yet available for 2024
+#  ttZ,TTZ_Zto2L,VVV #not yet available for 2024
+  WWZ,WWZ,VVV
+  WZZ,WZZ,VVV
+  ZZZ,ZZZ,VVV
+)
+streams="EGamma0 EGamma1 MuonEG Muon0 Muon1"
+suffixes=("")
+
+# Begin output
+outfile=ZZSelectionsTightLeps_temp.json
+echo "{" > $outfile
+
+# MC
+for sim in "${sims[@]}"; do
+  name=$(cut -d , -f 1 <<< $sim)
+  plotgroup=$(cut -d , -f 3 <<< $sim)
+
+  for suff in "${suffixes[@]}"; do
+    dirs=( ${srcdir}/*-${name}${suff}-ZZ4l${year}*/ )
+    if [[ ${#dirs[@]} -lt 1 || ! -d ${dirs[0]} ]]; then
+      echo Skipping ${name}${suff}
+      continue
+    fi
+    echo ${name}${suff}
+
+    echo -e "    \"${name}${suff}\" : {" >> $outfile
+    echo -e "      \"file_path\" : \"${dirs[0]}*.root\"," >> $outfile
+    echo -e "      \"plot_group\" : \"${plotgroup}\"" >> $outfile
+    echo -e "    }," >> $outfile
+  done
+done
+
+# Data
+for stream in $streams; do
+  for dir in ${srcdir}/*-data_${stream}_Run${year}*; do
+    if [[ ! -d $dir ]]; then
+     echo Skipping ${stream}
+     continue
+    fi
+
+    name=${dir#*data_}
+    name=data_${name%-ZZ4l*}
+    echo $name
+
+    echo -e "    \"${name}\" : {" >> $outfile
+    echo -e "      \"file_path\" : \"${dir}/*.root\"," >> $outfile
+    echo -e "      \"plot_group\" : \"data-${year}\"" >> $outfile
+    echo -e "    }," >> $outfile
+  done
+done
+
+if $overwrite; then
+  cp $outfile LooseLeptons.json
+  sed -i "$ s/.$//" LooseLeptons.json
+  echo "}" >> LooseLeptons.json
+fi
+
+echo -e "    \"AllData\" : {" >> $outfile
+echo -e "        \"file_path\" : \"\"," >> $outfile
+echo -e "        \"plot_group\" : \"data_all\"" >> $outfile
+echo -e "    }," >> $outfile
+echo -e "    \"DataEWKCorrected\" : {" >> $outfile
+echo -e "        \"file_path\" : \"\"," >> $outfile
+echo -e "        \"plot_group\" : \"nonprompt\"" >> $outfile
+echo -e "    }" >> $outfile
+echo "}" >> $outfile
+
+echo
+if $overwrite; then
+  mv $outfile ${outfile/_temp/}
+  echo Output files: ${outfile/_temp/} and LooseLeptons.json
+else
+  echo Output file: $outfile
+fi
---
+++ FileInfo/ZZ4l2024/makeJSON_ZL.sh
@@ -0,0 +1,82 @@
+year=2024
+srcdir=/hdfs/store/user/marquez/ZplusL${year}-skimmed
+overwrite=true
+
+[[ -d $1 ]] && srcdir=$1
+srcdir=${srcdir%/}
+
+sims=(
+  #member_name,campaign_prefix,plot_group
+  zz4l-powheg,ZZto4L,qqZZ-powheg
+  zzjj4l-ewk,ZZJJto4L,qqZZjj-ewk
+  ggHZZ,GluGluH-Hto2Zto4L,HZZ-signal
+#  ggZZ4e,GluGlu*Continto2Zto4E,ggZZ #not yet available for 2024
+#  ggZZ4m,GluGlu*Continto2Zto4Mu,ggZZ #not yet available for 2024
+#  ggZZ4t,GluGlu*Continto2Zto4Tau,ggZZ #not yet available for 2024
+#  ggZZ2e2mu,GluGlu*Continto2Zto2E2Mu,ggZZ #not yet available for 2024
+#  ggZZ2e2tau,GluGlu*Continto2Zto2E2Tau,ggZZ #not yet available for 2024
+#  ggZZ2mu2tau,GluGlu*Continto2Zto2Mu2Tau,ggZZ #not yet available for 2024
+#  ttZ,TTZ_Zto2L,VVV #not yet available for 2024
+  WWZ,WWZ,VVV
+  WZZ,WZZ,VVV
+  ZZZ,ZZZ,VVV
+  #fakes only
+  wz3lnu-powheg,WZto3LNu,wz3lnu-powheg
+  tt2l2nu-powheg,TT*2L2Nu,top
+)
+streams="EGamma0 EGamma1 MuonEG Muon0 Muon1"
+suffixes=("")
+
+# Begin output
+outfile=ZplusLSkim_temp.json
+echo "{" > $outfile
+
+# MC
+for sim in "${sims[@]}"; do
+  name=$(cut -d , -f 1 <<< $sim)
+  plotgroup=$(cut -d , -f 3 <<< $sim)
+
+  for suff in "${suffixes[@]}"; do
+    dirs=( ${srcdir}/*-${name}${suff}-ZplusL${year}*/ )
+    if [[ ${#dirs[@]} -lt 1 || ! -d ${dirs[0]} ]]; then
+      echo Skipping ${name}${suff}
+      continue
+    fi
+    echo ${name}${suff}
+
+    echo -e "    \"${name}${suff}\" : {" >> $outfile
+    echo -e "      \"file_path\" : \"${dirs[0]}*.root\"," >> $outfile
+    echo -e "      \"plot_group\" : \"${plotgroup}\"" >> $outfile
+    echo -e "    }," >> $outfile
+  done
+done
+
+# Data
+for stream in $streams; do
+  for dir in ${srcdir}/*-data_${stream}_Run${year}*; do
+    if [[ ! -d $dir ]]; then
+     echo Skipping ${stream}
+     continue
+    fi
+
+    name=${dir#*data_}
+    name=data_${name%%-ZplusL*}
+    echo $name
+
+    echo -e "    \"${name}\" : {" >> $outfile
+    echo -e "      \"file_path\" : \"${dir}/*.root\"," >> $outfile
+    echo -e "      \"plot_group\" : \"data-${year}\"" >> $outfile
+    echo -e "    }," >> $outfile
+  done
+done
+
+sed -i "$ s/.$//" $outfile
+echo "}" >> $outfile
+
+echo
+if $overwrite; then
+  mv $outfile ${outfile/_temp/}
+  echo Output files: ${outfile/_temp/}
+else
+  echo Output file: $outfile
+fi
