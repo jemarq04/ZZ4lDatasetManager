b2ab36bb3a9ec43800ba8fcc5558b051c8b26652
Justin Marquez <justin.marquez@wisc.edu>
2025-11-15 17:52:40 -0600

    add option to skip values in combined list of years (Run3Combined)

--- FileInfo/montecarlo/makeJSON.py
+++ FileInfo/montecarlo/makeJSON.py
@@ -7,21 +7,25 @@ def main():
     with open("../../luminosityMap.json") as infile:
         lumi_info = json.load(infile)
 
-    lumi_map = {}
-    for year in lumi_info["Run3Combined"]["years"]:
-        if "eras" in lumi_info[year]:
-            lumi_map[int(year)] = {f"_{era}": lumi for era,lumi in lumi_info[year]["eras"].items()}
-        else:
-            lumi_map[int(year)] = {"": lumi_info[year]["lumi"]}
-
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-i", "--base-file", dest="infile", default="base.txt", help="template file used to create full JSON")
     parser.add_argument("-o", "--outfile", default="montecarlo_zzanalysis.json", help="output file")
     parser.add_argument("-f", "--force", action="store_true", help="overwrite output file if it exists")
     parser.add_argument("-k", "--k-factors", action="store_true", help="include k-factors")
     parser.add_argument("-c", "--combined", action="store_true", help="provide xsecs with year identifiers for combined plot group")
+    parser.add_argument("-a", "--analysis", default="Run3Combined", help="name of combined-year analysis to create xsec file (default: Run3Combined)")
+    parser.add_argument("-s", "--skip", type=lambda x: [i.strip() for i in x.split(",")], default=[], help="comma-separated list of years to skip")
 
     args = parser.parse_args()
+
+    lumi_map = {}
+    for year in lumi_info[args.analysis]["years"]:
+        if year in args.skip:
+            continue
+        if "eras" in lumi_info[year]:
+            lumi_map[int(year)] = {f"_{era}": lumi for era,lumi in lumi_info[year]["eras"].items()}
+        else:
+            lumi_map[int(year)] = {"": lumi_info[year]["lumi"]}
 
     if not os.path.isfile(args.infile):
         parser.error(f'invalid input file: {args.infile}')
