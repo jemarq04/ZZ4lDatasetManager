5d397b21bf0eb32a6c7265fbe07116bd51f3d478
Justin Marquez <justin.marquez@wisc.edu>
2026-01-23 19:30:19 -0600

    apply ruff formatting

--- Utilities/python/config_object.py
+++ Utilities/python/config_object.py
@@ -1,41 +1,42 @@
 import ROOT
 import array
 
+
 class ConfigObject:
     def __init__(self, data):
         self.data = data
+
     def getObject(self, object_name, title=""):
-        initialize = self.data[object_name]['Initialize']
-        if "TH1" in initialize['type']:
+        initialize = self.data[object_name]["Initialize"]
+        if "TH1" in initialize["type"]:
             if "varbins" not in initialize:
-                tObject = ROOT.TH1D(object_name, title,
-                    initialize['nbins'], initialize['xmin'],
-                    initialize['xmax'])
+                tObject = ROOT.TH1D(object_name, title, initialize["nbins"], initialize["xmin"], initialize["xmax"])
             else:
-                tObject = ROOT.TH1D(object_name, object_name,
-                    initialize['nbins'],
-                    array.array('d', initialize['varbins']))
+                tObject = ROOT.TH1D(
+                    object_name, object_name, initialize["nbins"], array.array("d", initialize["varbins"])
+                )
             tObject.SetDirectory(ROOT.gROOT)
-        elif initialize['type'] == "TCanvas":
-            tObject = ROOT.TCanvas(object_name, object_name,
-                                initialize['ww'], initialize['wh'])
+        elif initialize["type"] == "TCanvas":
+            tObject = ROOT.TCanvas(object_name, object_name, initialize["ww"], initialize["wh"])
         else:
             tObject = ""
         return tObject
+
     def deepGetattr(self, obj, attr):
         """Recurses through an attribute chain to get the ultimate value.
-            via http://pingfive.typepad.com/blog/2010/04/deep-getattr-python-function.html"""
+        via http://pingfive.typepad.com/blog/2010/04/deep-getattr-python-function.html"""
         try:
             return float(attr)
         except ValueError:
-            return self.evaluateNested(getattr, attr.split('.'), obj)
+            return self.evaluateNested(getattr, attr.split("."), obj)
+
     def evaluateNested(self, func, iterable, start=None):
         it = iter(iterable)
         if start is None:
             try:
                 start = next(it)
             except StopIteration as err:
-                raise TypeError('reduce() of empty sequence with no initial value') from err
+                raise TypeError("reduce() of empty sequence with no initial value") from err
         accum_value = start
         for x in iterable:
             split = str(x).strip(")").split("(")
@@ -64,13 +65,14 @@ class ConfigObject:
                         root_val += int(values[1])
                     elif "-" in param_str:
                         values = [x.strip() for x in expr.split("-")]
-                        root_val =self.deepGetattr(ROOT, values[0])
+                        root_val = self.deepGetattr(ROOT, values[0])
                         root_val -= int(values[1])
                     else:
                         root_val = self.deepGetattr(ROOT, expr)
                     param = root_val
                 parsed_params.append(param)
             self.deepGetattr(tObject, function_call)(*parsed_params)
+
     def getHistCanvas(self, hist_name):
         canvas = self.getCanvas()
         hist = self.getObject(hist_name)
@@ -78,11 +80,13 @@ class ConfigObject:
         self.setAtrributes(hist)
         hist.Draw()
         return canvas
+
     def getCanvas(self):
         canvas = self.getObject("Canvas")
         self.setAttributes(canvas, "Canvas")
         canvas.cd()
         return canvas
+
     def getListOfHists(self):
         list_of_hists = []
         for key in self.data:
--- extratools/copydatafiles_multithread_v2.py
+++ extratools/copydatafiles_multithread_v2.py
@@ -1,4 +1,4 @@
-import subprocess #getstatusoutput() move to subprocess for python 3
+import subprocess  # getstatusoutput() move to subprocess for python 3
 import os
 import threading
 import time
@@ -6,52 +6,52 @@ import concurrent.futures
 import queue
 
 
+commandlist = []
+maxnum = -1
+count = 0
 
-commandlist=[]
-maxnum=-1
-count=0
-
-lock=threading.Lock()
+lock = threading.Lock()
 
 t0 = time.time()
+
 
 def copyfile(command):
     recordname = command[1]
     out = subprocess.getstatusoutput(command[0])
 
-
-    statlist.put(recordname+' '+str(out[0])+'\n')
-    if out[0]!=0:
-        errlist.put(recordname+'\n'+str(out[1])+'\n')
+    statlist.put(recordname + " " + str(out[0]) + "\n")
+    if out[0] != 0:
+        errlist.put(recordname + "\n" + str(out[1]) + "\n")
     count2 = statlist.qsize()
-    print('(approx) processed %s/%s %s '%(count2,(count),out[1].split('\r')[-1]))
+    print("(approx) processed %s/%s %s " % (count2, (count), out[1].split("\r")[-1]))
 
-with open('rootfilelist.txt') as flist:
+
+with open("rootfilelist.txt") as flist:
     for line in flist:
-        count+=1
-        if maxnum>0 and count>maxnum:
+        count += 1
+        if maxnum > 0 and count > maxnum:
             break
 
         path = line.strip()
-        dirname = path.split('/')[1]
-        recordname = path.split('/',1)[1]
+        dirname = path.split("/")[1]
+        recordname = path.split("/", 1)[1]
         if os.path.exists(recordname):
             continue
         if not os.path.isdir(dirname):
             os.mkdir(dirname)
-        command = 'xrdcp root://cmsxrootd.hep.wisc.edu//store/user/hhe62/'+path+' '+dirname
-        commandlist.append([command,recordname])
+        command = "xrdcp root://cmsxrootd.hep.wisc.edu//store/user/hhe62/" + path + " " + dirname
+        commandlist.append([command, recordname])
 
-errlist = queue.Queue(maxsize=(count+2))
-statlist = queue.Queue(maxsize=(count+2))
+errlist = queue.Queue(maxsize=(count + 2))
+statlist = queue.Queue(maxsize=(count + 2))
 
-with open('copy_error.txt','w') as ferr:
-    with open('copy_status.txt','w') as fstat:
+with open("copy_error.txt", "w") as ferr:
+    with open("copy_status.txt", "w") as fstat:
         with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-            executor.map(copyfile,commandlist)
+            executor.map(copyfile, commandlist)
         while not statlist.empty():
             fstat.write(statlist.get())
         while not errlist.empty():
             ferr.write(errlist.get())
 
-print('time spent: %s'%(time.time()-t0))
+print("time spent: %s" % (time.time() - t0))
