e6be956bb7dfdb7469d2998ffe6578be89bf0fbf
Justin Marquez <justin.marquez@wisc.edu>
2026-01-26 14:36:26 -0600

    update makeJSON scripts

--- FileInfo/ZZ4l2022/makeJSON.sh
+++ FileInfo/ZZ4l2022/makeJSON.sh
@@ -19,7 +19,6 @@ sims=(
   ggZZ2e2mu,GluGlu*Continto2Zto2E2Mu,ggZZ
   ggZZ2e2tau,GluGlu*Continto2Zto2E2Tau,ggZZ
   ggZZ2mu2tau,GluGlu*Continto2Zto2Mu2Tau,ggZZ
-  #ttZ,TTZ_Zto2L,VVV
   ttZ,TTLL,VVV
   WWZ,WWZ,VVV
   WZZ,WZZ,VVV
@@ -31,8 +30,9 @@ sims=(
 )
 streams="EGamma MuonEG Muon SingleMuon DoubleMuon"
 eras=(
-  _preEE,Run3Summer22Mini
-  _postEE,Run3Summer22EEMini
+  #suffix,campaign_conditions,custom_globaltag
+  _preEE,Run3Summer22Mini,
+  _postEE,Run3Summer22EEMini,
 )
 
 echo "{" > $outfile
@@ -46,6 +46,7 @@ for sim in ${sims[@]}; do
   for era in "${eras[@]}"; do
     suff=$(cut -d , -f 1 <<< $era)
     conditions=$(cut -d , -f 2 <<< $era)
+    [[ $campaign =~ ^Custom ]] && conditions="*$(cut -d , -f 3 <<< $era)"
 
     fdirs=( ${srcdir}/${campaign}*/${conditions}*/ )
     if [[ ${#fdirs[@]} -lt 1 || ! -d ${fdirs[0]} ]]; then
